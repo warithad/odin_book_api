a65ab1cab0975e53be0edce29962c5a3b97f259a
warith <warith.ad02@gmail.com>
2023-03-20 01:15:08 +0100

    Add route to delete user account to userRouter.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -10,6 +10,7 @@ router.put('/friends/accept', userController.accept_request_PUT)
 router.delete('/friends/reject', userController.reject_request_DELETE)
 
 
+router.delete('/delete_account', userController.delete_user_DELETE)
 router.put('/update', userController.update_user_PUT)
 router.get('/:id', userController.get_user_GET)
 router.get('/', userController.all_users_GET)
